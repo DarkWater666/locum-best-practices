01d4d3f7612abfdec0594cefc6cf817923de13bd
Денис Арушанов <denis.arushanov@saltpepper.ru>
2016-02-01 11:26:52 +0300

    add generators && tasks

---
+++ lib/capistrano/locum.rb
@@ -0,0 +1,5 @@
+require 'capistrano/locum/defaults'
+require 'capistrano/locum/assets'
+require 'capistrano/locum/remote'
+require 'capistrano/locum/sidekiq'
+require 'capistrano/locum/ssh'
---
+++ lib/capistrano/locum/assets.rb
@@ -0,0 +1,1 @@
+load File.expand_path('../../tasks/assets.cap', __FILE__)
